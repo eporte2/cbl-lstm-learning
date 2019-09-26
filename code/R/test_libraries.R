c99cdd5d4d3584b37c8417e2866b924709ad909f
evap <portelance.eva@gmail.com>
2019-09-26 11:29:03 -0700

    adding test scripts to check dependencies on cluster

---
+++ code/R/test_libraries.R
@@ -0,0 +1,14 @@
+
+print("Has R, check Libraries:")
+
+library(tools)
+library(tidyverse)
+library(glue)
+library(broom)
+library(broom.mixed)
+library(langcog)
+library(stringr)
+library(lme4)
+library(modelr)
+library(purrr)
+
